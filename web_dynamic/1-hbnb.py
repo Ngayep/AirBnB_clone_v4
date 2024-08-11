f699fe0952ec73b52162f627f7eb66b35df629b6
Ngayep <courtesyofjess@gmail.com>
2024-08-11 16:47:58 +0100

    task 2 completed

---
+++ web_dynamic/1-hbnb.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Starts a Flask web application for 1-hbnb """
+from flask import Flask, render_template
+from models import storage
+import uuid
+
+
+app = Flask(__name__)
+
+
+@app.route('/1-hbnb', strict_slashes=False)
+def display_hbnb():
+    """ Route to display 1-hbnb """
+    cache_id = uuid.uuid4()
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    return render_template('1-hbnb.html', states=states, amenities=amenities,
+                           cache_id=cache_id)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Close the storage session after each request """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
